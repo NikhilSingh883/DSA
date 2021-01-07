43a4da91d78e1ab971600d4ec43c3aae86865ed5
NikhilSingh883 <56299991+NikhilSingh883@users.noreply.github.com>
2021-01-07 16:01:30 +0530

    BST

--- Binary Search Tree/BinarySearchTreeAllFunctions.cpp
+++ Binary Search Tree/BinarySearchTreeAllFunctions.cpp
@@ -186,3 +186,200 @@ Node *binaryTreeToBST (Node *root)
         return build(preorder,i,INT_MIN,INT_MAX);
     }
 
+// balance BST
+    void storeTree(TreeNode* root,vector<TreeNode*> &t){
+        if(!root) return;
+        
+        storeTree(root->left,t);
+        t.push_back(root);
+        storeTree(root->right,t);
+    }
+    TreeNode* balanceTree(vector<TreeNode*> tree,int st,int en){
+        
+        if(st > en) return NULL;
+        
+        int mid = (st+en)/2;
+        
+        TreeNode* root =  tree[mid];
+        
+        root->left = balanceTree(tree,st,mid-1);
+        root->right = balanceTree(tree,mid+1,en);
+        
+        return root;
+    }
+    
+    
+    TreeNode* balanceBST(TreeNode* root) {
+        vector<TreeNode*> tree;
+        storeTree(root,tree);
+        int n = tree.size();
+        return balanceTree(tree,0,n-1);
+    }
+
+// merge two BST
+
+    void storeInorder(TreeNode* root,vector<int> &t){
+        if(!root) return;
+        
+        storeInorder(root->left,t);
+        t.push_back(root->data);
+        storeInorder(root->right,t);
+    }
+void merge(vector<int> a,vector<int> b,vector<int> R,int m,int n){
+    int i=0,j=0;
+
+    while(i<m && j<n){
+        if(a[i] < b[j]){
+            R.push_back(a[i]);
+            i++;
+        }
+        else {
+            R.push_back(b[j]);
+            j++;
+        }
+    }
+
+    while(i<m){
+        R.push_back(a[i]);
+        i++;
+    }
+    while(j<n){
+        R.push_back(b[j]);
+        j++;
+    }
+}
+    TreeNode* balanceTree(vector<int> tree,int st,int en){
+        
+        if(st > en) return NULL;
+        
+        int mid = (st+en)/2;
+        
+        TreeNode* root =  TreeNode(tree[mid]);
+        
+        root->left = balanceTree(tree,st,mid-1);
+        root->right = balanceTree(tree,mid+1,en);
+        
+        return root;
+    }
+
+Node* mergeTrees(Node* root1,Node* root2,int m,int n){
+    vector<int> r1,r2,R;
+
+    storeInorder(root1,r1);
+    storeInorder(root2,r1);
+
+    merge(r1,r2,R,m,n);
+
+    return balancedTree(R,0,m + n - 1);
+
+}
+
+// kth largest element
+int ans;
+
+void kthLargestUtil(Node *root, int k, int &c) 
+{ 
+
+    if (root == NULL || c >= k) 
+        return; 
+
+    kthLargestUtil(root->right, k, c); 
+
+    c++; 
+
+    if (c == k) 
+    { 
+        ans = root->data; 
+        return; 
+    } 
+
+    kthLargestUtil(root->left, k, c); 
+}
+
+int kthLargest(Node *root, int K)
+{
+    int c=0;
+    kthLargestUtil(root,K,c);
+    
+    return ans;
+}
+
+// kth smallest element
+Node* kthSmallest(Node* root, int& k)
+{
+    // base case
+    if (root == NULL)
+        return NULL;
+ 
+    // search in left subtree
+    Node* left = kthSmallest(root->left, k);
+ 
+    // if k'th smallest is found in left subtree, return it
+    if (left != NULL)
+        return left;
+ 
+    // if current element is k'th smallest, return it
+    k--;
+    if (k == 0)
+        return root;
+ 
+    // else search in right subtree
+    return kthSmallest(root->right, k);
+}
+
+int KthSmallestElement(Node *root, int K)
+{
+    //add code here.
+    Node *n = kthSmallest(root,K);
+    if(n==NULL) return -1;
+    return n->data;
+}
+
+// count of pairs with sum x
+int countPairs(Node* root1, Node* root2, int x)
+{
+    if (root1 == NULL || root2 == NULL)
+        return 0;
+    int count =0;
+    stack<Node*> st1, st2;
+    Node* top1, *top2;
+    
+    while(1){
+        while(root1!=NULL){
+            st1.push(root1);
+            root1=root1->left;
+        }
+        while(root2!=NULL){
+            st2.push(root2);
+            root2=root2->right;
+        }
+        
+        if(st1.empty() || st2.empty())
+            break;
+            
+        top1 = st1.top();
+        top2 = st2.top();
+        
+        if((top1->data + top2->data) == x){
+            count++;
+            
+            st1.pop();
+            st2.pop();
+            
+            root1 = top1->right;
+            root2 = top2->left;
+        }
+        else if((top1->data + top2->data) < x){
+            st1.pop();
+            
+            root1 = top1->right;
+        }
+        else{
+            st2.pop();
+            root2 = top2->left;
+        }
+    }
+    
+    return count;
+}
+
