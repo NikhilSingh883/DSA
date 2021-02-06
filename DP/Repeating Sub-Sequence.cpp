af4758126eb90fdee40dfb41c8317a0298b75dc0
NikhilSingh883 <56299991+NikhilSingh883@users.noreply.github.com>
2021-02-06 23:15:32 +0530

    rss

---
+++ DP/Repeating Sub-Sequence.cpp
@@ -0,0 +1,30 @@
+int dp[1006][1006];
+
+int lcs(string a,string b,int n,int m){
+    if(n<=0 || m<=0) return 0;
+    
+    if(dp[n][m]!=-1) return dp[n][m];
+    
+    if(a[n-1]==b[m-1])
+        return dp[n][m] = 1 + lcs(a,b,n-1,m-1);
+        
+    return dp[n][m] = max(lcs(a,b,n-1,m),lcs(a,b,n,m-1));
+}
+
+int Solution::anytwo(string A) {
+
+    string B = A;
+    int n = A.size(),m=B.size();
+    memset(dp,0,sizeof dp);
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(A[i-1]==B[j-1] && i!=j)
+                dp[i][j] = 1 + dp[i-1][j-1];
+            else 
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+    
+    return  dp[n][m]>=2;
+}
